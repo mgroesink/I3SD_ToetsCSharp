1dd1b7fe4f77d14ad45ea153238c810ddb40cf0f
Marcel Roesink <mroesink@rocvantwente.nl>
2024-03-26 15:32:47 +0100

    Some changes to Program.cs

--- ToetsCSharp/Program.cs
+++ ToetsCSharp/Program.cs
@@ -4,7 +4,11 @@
     {
         // Voeg jezelf als een nieuwe persoon toe
 
+        // Toon info van de persoon die je net hebt toegevoegd door de ToString methode te gebruiken
+
         // Voeg een nieuwe auto toe met als eigenaar de persoon die je net hebt toegevoegd
+
+        // Toon info van de auto die je net hebt toegevoegd door de Info methode te gebruiken
 
         // Geef alle gegevens van de auto weer inclusief de gegevens van de eigenaar
         // Laat de gegevens van de eigenaar één tab inspringen
